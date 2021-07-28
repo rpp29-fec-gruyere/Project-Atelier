938d75b5e3faf230c6dbbd1b71d57a3aadbc9b61
William Hutagaol <williamhutagaol@Williams-MacBook-Pro.local>
2021-07-27 18:34:17 -0700

    update

--- client/src/components/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+
 import QandAForm from './QandAForm.jsx';
 import QandAList from './QandAList.jsx';
 import QandAButtons from './QandAButtons.jsx';
 
-const QuestionsAndAnswers = ({ questionsAndAnswers }) => {
-  return (
-    <div data-testid='QuestionsAndAnswer' className='q-and-a-container'>
-      <span className='q-and-a-title'>QUESTIONS AND ANSWERS</span>
-      <QandAForm />
-      <QandAList />
-      <QandAButtons />
+class QuestionsAndAnswers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qAndAList: []
+    };
+  }
 
-    </div>
-  );
-};
+  render() {
+    return (
+      <div data-testid='QuestionsAndAnswer' className='q-and-a-container'>
+        <span className='q-and-a-title'>QUESTIONS AND ANSWERS</span>
+        <QandAForm />
+        <QandAList />
+        <QandAButtons />
+
+      </div>
+    );
+  }
+}
 
 export default QuestionsAndAnswers;
+
+
