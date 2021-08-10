49dcca9f8fa16f599a40ef8bb64b768a157a8a66
William Hutagaol <williamhutagaol@Williams-MacBook-Pro.local>
2021-08-10 11:26:28 -0700

    Resized image

--- client/src/components/QandAItem.jsx
+++ client/src/components/QandAItem.jsx
@@ -12,16 +12,18 @@ const QandAItem = ({ questionsAndAnswers }) => (
             item.answers.slice(0, 2).map((answer, i) => (
               <div key={i}>
                 <div className='q-and-a-item-answer'><b>A:</b> {answer.body}</div>
-                <div className='q-and-a-item-answer-pic'>
+                <div className='q-and-a-item-answer-pics'>
                   {
                     answer.photos.map((photo, i) => (
-                      <img src={photo} key={i}></img>
+                      <div id="q-an-a-item-img-container" key={i}>
+                        <img src={photo} width='100' height='70' ></img>
+                      </div>
                     ))
                   }
 
                 </div>
                 <div className='q-and-a-item-user'> by: {answer.answerer_name + ', ' + new Date(answer.date).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
-                | Helpful? <button className='q-and-a-question-helpful-answer'>Yes</button>({answer.helpfulness}) | <button className='q-and-a-question-report'>Report</button>
+                  | Helpful? <button className='q-and-a-question-helpful-answer'>Yes</button>({answer.helpfulness}) | <button className='q-and-a-question-report'>Report</button>
                 </div>
               </div>
             ))
