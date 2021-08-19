7c544384ce1f707da699ca057fc370f775b5a78a
Ryan Lott <ryanclott@outlook.com>
2021-08-19 17:49:44 -0500

    Removed commented out Class Component

--- client/src/components/ReviewSection.jsx
+++ client/src/components/ReviewSection.jsx
@@ -3,68 +3,6 @@ import RatingsBreakdown from './RatingBreakdown.jsx';
 import ReviewList from './ReviewList.jsx';
 import AddReviewModal from './AddReviewModal.jsx';
 
-// class ReviewSection extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showAddReviewModal: false,
-//       reviews: []
-//     };
-//   }
-
-//   showAddReviewModal() {
-//     this.setState({showAddReviewModal: true});
-//   }
-
-//   closeAddReviewModal() {
-//     this.setState({showAddReviewModal: false});
-//   }
-
-//   fetchReviews(sortOption = 'relevance') {
-//     console.log('this.props.itemInfo.id', this.props.id);
-//     this.props.handleFetch({
-//       endpoint: 'reviews',
-//       params: {
-//         'sort': sortOption,
-//         'product_id': this.props.itemInfo.id
-//       }
-//     },
-//     data => {
-//       this.setState({reviews: data.results});
-//       console.log('Hi!');
-//     });
-//   }
-
-//   componentDidMount() {
-//     this.fetchReviews();
-//   }
-
-//   render() {
-
-    
-//     return (
-//       <div data-testid="reviewSection" className="reviewSection" id="reviewSection">
-//         {this.state.showAddReviewModal ? 
-//           <AddReviewModal 
-//             itemInfo={this.props.itemInfo}
-//             characteristics={this.props.reviewData.meta.characteristics}
-//             handleClose={this.closeAddReviewModal.bind(this)}
-//             handlePost={this.props.handlePost}
-//           ></AddReviewModal> : null}
-//         <span className="widgetHeader">Ratings &#38; Reviews</span>
-//         <div id="mainReviewSection">
-//           <RatingsBreakdown metaData={this.props.reviewData ? this.props.reviewData.meta : false}/>
-//           <ReviewList 
-//             reviews={this.props.reviewData ? this.props.reviewData.allReviews : false} 
-//             showAddReviewModal={this.showAddReviewModal.bind(this)} 
-//             handlePut={this.props.handlePut}
-//             fetchReviews={this.fetchReviews.bind(this)}/>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 const ReviewSection = props => {
   const [reviewModalDisplay, setReviewModalDisplay] = useState(false);
   const [reviews, setReviews] = useState([]);
