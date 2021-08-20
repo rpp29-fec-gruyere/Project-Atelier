2e011230abf89459a717c962563459db970ed99f
Ryan Lott <ryanclott@outlook.com>
2021-08-20 08:37:55 -0500

    Show more btn implemented (no-internet/untested)

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Stars from './Stars.jsx';
 import $ from 'jquery';
 
 const Review = props => {
   let reviewDate = new Date(props.reviewInfo.date).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
   let response = undefined;
+
+  const [showFullReviewBody, setShowFullReviewBody] = useState(false);
 
   if (props.reviewInfo.response) {
     response = 
@@ -24,6 +26,12 @@ const Review = props => {
     setTimeout(() => { props.fetchReviews(false); }, 50);
   };
 
+  let handleShowMoreBtnClick = () => {
+    setShowFullReviewBody(true);
+  };
+
+  let reviewBodyShowMoreBtn = <span id="reviewBodyShowMoreBtn" onClick={() => handleShowMoreBtnClick()}>Show more</span>;
+
   return (
     <div className="review">
       <div className="reviewHeader">
@@ -31,7 +39,9 @@ const Review = props => {
         <div className="reviewUser">{props.reviewInfo.reviewer_name}, {reviewDate}</div>
       </div>
       <span>{props.reviewInfo.summary}</span>
-      <p>{props.reviewInfo.body}</p>
+      <p>{showFullReviewBody ? 
+        props.reviewInfo.body : 
+        props.reviewInfo.body.slice(0, 251) + '...' + reviewBodyShowMoreBtn}</p>
       {imgs.length > 0 ? <div className="reviewPhotoContainer">
         {imgs}
       </div> : undefined}
