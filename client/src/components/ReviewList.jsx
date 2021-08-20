a56a688bf2a9cdddc54c4dd424224bcfd13d06f5
Layla Grace <laylagrace212@gmail.com>
2021-08-20 18:50:09 -0400

    Added text compression

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -40,7 +40,7 @@ const ReviewList = props => {
   return (
     <div data-testid="reviewList" className="reviewList">
       <div id="sortOptions">
-        <span>{reviewCount} reviews, sorted by 
+        <span>{reviewCount} reviews, sorted by
           <select name="sortOptions" id="sortOptions" value={sortOption} onChange={(e) => { handleSortChange(e); }}>
             <option value="relevant">relevant</option>
             <option value="newest">newest</option>
